337c1e9a588e3210dd122c66506e78efbde9f4b1
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-09-06 18:11:36 -0300

    feat: Package (Home, Map) HomeScreen, MapScreen, Scaffold, FloatingButton,

---
+++ app/src/main/java/com/example/turistaapp/home/ui/HomeScreens.kt
@@ -0,0 +1,55 @@
+package com.example.turistaapp.home.ui
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.turistaapp.Greeting
+import com.example.turistaapp.ui.theme.TuristaAppTheme
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+
+
+    Scaffold (
+        bottomBar = {},
+        floatingActionButton = {
+               FloatingScreen()
+        },
+    ){
+
+    }
+}
+
+@Composable
+fun FloatingScreen() {
+    FloatingActionButton(onClick = { }) {
+        Icon(Icons.Default.Add, contentDescription = "Add")
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPrev() {
+    TuristaAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            HomeScreen()
+        }
+    }
+
+}
