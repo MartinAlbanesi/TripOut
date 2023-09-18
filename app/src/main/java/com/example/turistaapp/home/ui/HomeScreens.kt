5d5f708a92720759dfce46cd5f6f100c83f8b85a
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-09-17 23:36:28 -0300

    feat: implement Dialog in the items from bottomSheet

--- app/src/main/java/com/example/turistaapp/home/ui/HomeScreens.kt
+++ app/src/main/java/com/example/turistaapp/home/ui/HomeScreens.kt
@@ -1,33 +1,35 @@
 package com.example.turistaapp.home.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Cancel
 import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.turistaapp.R
 import com.google.maps.android.compose.MapUiSettings
 
@@ -43,6 +45,10 @@ fun HomeScreen(paddingValues: PaddingValues) {
         )
     }
 
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
+
     val paddingFromScaffold by remember {
         mutableStateOf(paddingValues.calculateBottomPadding() * 1.5f)
     }
@@ -54,14 +60,17 @@ fun HomeScreen(paddingValues: PaddingValues) {
         sheetContent = {
             Box(Modifier.padding(paddingValues)) {
                 Card() {
-                Text(text = "Descubre más viajes.", modifier = Modifier.padding(4.dp))
+                    Text(text = "Descubre más viajes.", modifier = Modifier.padding(4.dp))
                     LazyRow(modifier = Modifier.padding(4.dp)) {
                         items(10) {
                             TripItem(
                                 image = painterResource(id = R.drawable.ic_launcher_background),
                                 modifier = Modifier
+                                    .clickable {
+                                        showDialog = true
+                                    }
                                     .size(150.dp, 250.dp)
-                                    .padding(4.dp)
+                                    .padding(4.dp),
                             )
                         }
                     }
@@ -75,40 +84,91 @@ fun HomeScreen(paddingValues: PaddingValues) {
                 .padding(paddingValues)
                 .fillMaxSize()
         )
+        TripDialog(
+            title = "Titi",
+            km = "11.9Km",
+            image = painterResource(id = R.drawable.ic_launcher_background),
+            isShow = showDialog,
+            onDismiss = { showDialog = false },
+            onConfirm = { showDialog = false }
+        )
     }
 }
 
 @Composable
-fun <T> TripItem(image: T, modifier: Modifier = Modifier) {
-    when (image) {
-        is ImageVector -> {
-            Image(
-                image,
-                contentDescription = null,
-                modifier = modifier,
-                contentScale = ContentScale.FillBounds
-            )
-        }
+fun /*<T>*/ TripItem(
+    image: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Image(
+        image,
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = ContentScale.FillBounds
+    )
 
-        is Painter -> {
-            Image(
-                image,
-                contentDescription = null,
-                modifier = modifier,
-                contentScale = ContentScale.FillBounds
-            )
-        }
-
-        is ImageBitmap -> {
-            Image(
-                image,
-                contentDescription = null,
-                modifier = modifier,
-                contentScale = ContentScale.FillBounds
-            )
-        }
-    }
+//    when (image) {
+//        is ImageVector -> {
+//            Image(
+//                image,
+//                contentDescription = null,
+//                modifier = modifier.clickable { onClickItem },
+//                contentScale = ContentScale.FillBounds
+//            )
+//        }
+//
+//        is Painter -> {
+//            Image(
+//                image,
+//                contentDescription = null,
+//                modifier = modifier.clickable { onClickItem },
+//                contentScale = ContentScale.FillBounds
+//            )
+//        }
+//
+//        is ImageBitmap -> {
+//            Image(
+//                image,
+//                contentDescription = null,
+//                modifier = modifier.clickable { onClickItem },
+//                contentScale = ContentScale.FillBounds
+//            )
+//        }
+//    }
 
 
 }
 
+@Composable
+fun TripDialog(
+    title: String,
+    km: String,
+    image: Painter,
+    isShow: Boolean,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    if (isShow) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Box(Modifier.background(Color.Red)) {
+                Card {
+                    Text(text = title)
+                    Text(text = km)
+                    Image(image, contentDescription = null)
+                    Button(onClick = { onConfirm() }) {
+                        Text(text = "Confirmar Viaje")
+                    }
+                }
+
+                Icon(
+                    Icons.Outlined.Cancel,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .clickable { onDismiss() }
+                )
+
+            }
+        }
+    }
+}
