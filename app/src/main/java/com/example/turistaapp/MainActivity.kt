af9e723dfb3a757f397f927ab95e10a8954c9af9
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-09-19 16:16:39 -0300

    feat : change composable in the mainActivity

--- app/src/main/java/com/example/turistaapp/MainActivity.kt
+++ app/src/main/java/com/example/turistaapp/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen(mainViewModel = mainViewModel)
+                    Greeting(name = "Android")
                 }
             }
         }
