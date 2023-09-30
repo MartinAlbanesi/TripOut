73c29aa5dd81059f7b396ded1df789e1307ab50b
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-09-29 22:47:13 -0300

    feat: implement dagger hilt dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -85,4 +87,14 @@ dependencies {
 
     //Material extend icons
     implementation ("androidx.compose.material:material-icons-extended:1.4.3")
+    
+    //Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+
+}
+
+kapt {
+    correctErrorTypes = true
 }
