56c0876a64f6fa1bfe039bebae7ac6f4217e4c3a
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-09-29 22:54:12 -0300

    feat: implement dagger hilt in the project & modules (home, db, api)

--- app/src/main/java/com/example/turistaapp/main/MainViewModel.kt
+++ app/src/main/java/com/example/turistaapp/main/MainViewModel.kt
@@ -3,8 +3,13 @@ package com.example.turistaapp.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+
+): ViewModel() {
 
     private val _indexSelect = MutableLiveData(1)
     val indexSelect : LiveData<Int> = _indexSelect
