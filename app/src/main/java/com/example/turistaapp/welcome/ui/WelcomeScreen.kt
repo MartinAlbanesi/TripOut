4826bf7e714e21ed5bd11e604025de7bc99068fb
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-10-31 16:49:01 -0300

    feat: implement text field in the ViewTwo

--- app/src/main/java/com/example/turistaapp/welcome/ui/WelcomeScreen.kt
+++ app/src/main/java/com/example/turistaapp/welcome/ui/WelcomeScreen.kt
@@ -1,9 +1,7 @@
 package com.example.turistaapp.welcome.ui
 
-import android.util.Log
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
@@ -11,10 +9,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -23,27 +20,44 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.turistaapp.R
+import com.example.turistaapp.ui.theme.TuristaAppTheme
 import kotlinx.coroutines.launch
+
+@Preview
+@Composable
+fun Wea() {
+    TuristaAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            WelcomeScreen()
+        }
+    }
+}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -64,32 +78,36 @@ fun WelcomeScreen() {
 
     val buttonText = if (pagerState.currentPage == 0) "Siguiente" else "Empezar"
 
+    var value by remember { mutableStateOf("") }
+
 
     Box(
         modifier = Modifier
             .fillMaxSize()
     ) {
         Column(Modifier.fillMaxWidth()) {
-
-            Image(
-                painter = painterResource(id = R.drawable.logo_with_name),
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
+            Text(
+                text = "TRIP OUT",
                 modifier = Modifier
-                    .size(160.dp)
-                    .align(CenterHorizontally)
-                    .clip(CircleShape)
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .padding(top = 40.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 40.sp
             )
 
             HorizontalPager(
-                modifier = Modifier.weight(1f),
+                modifier = Modifier
+                    .weight(5f),
                 state = pagerState,
                 userScrollEnabled = false,
             ) { pager ->
                 when (pager) {
                     0 -> { ViewOne(stringResource(R.string.Welcome1)) }
 
-                    1 -> { ViewTwo(stringResource(R.string.Welcome2)) }
+                    1 -> { ViewTwo(value){
+                        value = it
+                    } }
                 }
             }
 
@@ -121,6 +139,7 @@ fun WelcomeScreen() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 32.dp, horizontal = 32.dp)
+                    .weight(1f)
             ) {
                 Text(text = buttonText)
             }
@@ -131,21 +150,53 @@ fun WelcomeScreen() {
 
 @Composable
 private fun ViewOne(text: String) {
-    Text(
-        text = text,
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-    )
+    Column(
+        Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_with_name),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(160.dp)
+                .align(CenterHorizontally)
+                .clip(CircleShape)
+        )
+        Text(
+            text = text,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+        )
+    }
 
 }
 
 @Composable
-private fun ViewTwo(text: String) {
+private fun ViewTwo(
+    value : String,
+    onValueChange: (String) -> Unit
+) {
     Column(
-        modifier = Modifier,
+        modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center
     ) {
-        ViewOne(text)
+        Text(
+            text = "Dinos tu nombre para comenzar",
+            Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        OutlinedTextField(
+            value = value,
+            onValueChange = {onValueChange(it)},
+            label = { Text(text = "Nombre") },
+            singleLine = true,
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)
+        )
     }
 }
 
