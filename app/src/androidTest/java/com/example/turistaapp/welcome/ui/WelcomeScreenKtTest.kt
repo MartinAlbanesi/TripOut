1fa831c422c0ffec3605953fdc2912ca1f74ca84
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-11-08 23:32:28 -0300

    feat: implement rule compose in WelcomeScreenTest

---
+++ app/src/androidTest/java/com/example/turistaapp/welcome/ui/WelcomeScreenKtTest.kt
@@ -0,0 +1,13 @@
+package com.example.turistaapp.welcome.ui
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Assert.*
+import org.junit.Rule
+
+class WelcomeScreenKtTest{
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+
+}
