be810b8105fade2e8a6c2f703f45b04672625376
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-11-10 16:07:37 -0300

    feat: implement booleans in the local datastore

--- app/src/main/java/com/example/turistaapp/core/utils/enums/DataStoreNames.kt
+++ app/src/main/java/com/example/turistaapp/core/utils/enums/DataStoreNames.kt
@@ -3,4 +3,6 @@ package com.example.turistaapp.core.utils.enums
 sealed class DataStoreNames(val name: String){
     object Name : DataStoreNames("name")
     object IsDarkMode : DataStoreNames("isDarkMode")
+    object IsMapTutorialComplete : DataStoreNames("isMapTutorialComplete")
+    object IsShakeGameTutorialComplete : DataStoreNames("isShakeGameTutorialComplete")
 }
