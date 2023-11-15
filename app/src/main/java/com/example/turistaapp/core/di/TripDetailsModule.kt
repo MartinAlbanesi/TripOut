bac9cb27799c70fd1091e91b84cebb7d9bfc0ab0
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-11-14 22:50:44 -0300

    feat: photo picker in tripDetails

--- app/src/main/java/com/example/turistaapp/core/di/TripDetailsModule.kt
+++ app/src/main/java/com/example/turistaapp/core/di/TripDetailsModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class TripDetails {
+class TripDetailsModule {
 
     @Singleton
     @Provides
--- app/src/main/java/com/example/turistaapp/setting/ui/TripDetail.kt
+++ app/src/main/java/com/example/turistaapp/setting/ui/TripDetail.kt
@@ -1,11 +1,13 @@
 package com.example.turistaapp.setting.ui
 
-import androidx.compose.foundation.layout.Arrangement
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -29,12 +31,20 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.turistaapp.map.domain.models.RouteModel
 
 
@@ -91,6 +101,7 @@ fun TripDetails(
 //    listaImagenes: List<String>,
 //    miembros: List<String>
     routeModel: RouteModel? = null,
+    settingViewModel: SettingViewModel = hiltViewModel()
 ) {
 //    Column(
 //        verticalArrangement = Arrangement.Center,
@@ -99,6 +110,23 @@ fun TripDetails(
 //        Column(
 //            modifier = Modifier.height(800.dp)  // aca es el largo del deslizante
 //        ) {
+
+
+    var selectedImageUris by remember {
+        mutableStateOf<List<String>>(emptyList())
+    }
+    val multiplePhotoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickMultipleVisualMedia(),
+        onResult = { uris ->
+            selectedImageUris += uris.map { it.toString() }
+            settingViewModel.updateImages(routeModel!!.trip!!.tripId, selectedImageUris)
+        }
+    )
+
+    LaunchedEffect(routeModel) {
+        Log.i("titi", routeModel?.trip?.images.toString())
+    }
+
     LazyColumn {
         item {
             Row(
@@ -143,7 +171,11 @@ fun TripDetails(
             LazyRow() {
                 item {
                     AssistChip(
-                        onClick = {},
+                        onClick = {
+                            multiplePhotoPickerLauncher.launch(
+                                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                            )
+                        },
                         label = {
                             Icon(
                                 imageVector = Icons.Default.ImageSearch, contentDescription = "",
@@ -383,35 +415,62 @@ fun TripDetails(
                 )
 
             }
-            //imagenes
+        }
+        //imagenes
 
+//            LazyRow(
+//                Modifier.fillMaxWidth()
+//            ) {
+        item {
             LazyRow(
                 Modifier.fillMaxWidth()
             ) {
                 items(routeModel!!.trip!!.images ?: emptyList()) { imagen ->
-                    AsyncImage(model = imagen, contentDescription = "")
+//            Log.i("titi", imagen)
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(imagen)
+                            .size(300, 300)
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = ""
+                    )
                 }
             }
+        }
+        item {
             Spacer(modifier = Modifier.size(9.dp))
             Text(
                 text = "Miembros",
                 modifier = Modifier,
                 fontSize = 26.sp,
             )
-            LazyRow() {
-                items(routeModel!!.trip!!.members ?: emptyList()) { chipValue ->
-                    AssistChip(
-                        onClick = {},
-                        label = { Text(chipValue, color = Color.White) },
-                        modifier = Modifier
-                            .padding(8.dp)
-                    )
-                }
-            }
         }
+        items(routeModel!!.trip!!.members ?: emptyList()) { chipValue ->
+            AssistChip(
+                onClick = {},
+                label = { Text(chipValue, color = Color.White) },
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+        }
+    }
+}
+
+//    LazyRow() {
+//        items(routeModel!!.trip!!.members ?: emptyList()) { chipValue ->
+//            AssistChip(
+//                onClick = {},
+//                label = { Text(chipValue, color = Color.White) },
+//                modifier = Modifier
+//                    .padding(8.dp)
+//            )
+//        }
+//    }
+//}
 //            }
 //
 //        }
 
-    }
-}
+//}
+//}
