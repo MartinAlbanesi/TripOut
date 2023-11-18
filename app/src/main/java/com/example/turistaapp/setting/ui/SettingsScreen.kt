12cb9d4be56fb2c2b76bcaa15c23867ab10424e8
MartinAlbanesi <martinalbanesi89@gmail.com>
2023-11-18 04:39:01 -0300

    style: better settings UI

--- app/src/main/java/com/example/turistaapp/setting/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/turistaapp/setting/ui/SettingsScreen.kt
@@ -1,99 +1,107 @@
 package com.example.turistaapp.setting.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
-import androidx.compose.material.icons.filled.ExitToApp
-import androidx.compose.material3.Card
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun SettingsScreen(darkTheme: Boolean = false, changeTheme: () -> Unit) {
+fun SettingsScreen(changeTheme: () -> Unit) {
     Column(
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxHeight()
-            .fillMaxWidth(),
-//     verticalAlignment = Alignment.CenterVertically
-
+            .fillMaxSize(),
     ) {
 //        SettingUser()
 //        Spacer(modifier = Modifier.size(21.dp))
         // Divider( thickness = 2.dp , modifier = Modifier.padding(4.dp))
-        SettingAppearance(darkTheme) { changeTheme() }
+        SettingAppearance { changeTheme() }
+        Spacer(modifier = Modifier.size(16.dp))
+        About()
+
 //        Spacer(modifier = Modifier.size(21.dp))
 //        SettingMore("MAS")
-        Spacer(modifier = Modifier.size(15.dp))
-        About()
-         Spacer(modifier = Modifier.size(560.dp))
-        SettingVersion()
+        Box(
+            contentAlignment = BottomCenter,
+            modifier = Modifier
+                .fillMaxSize(),
+        ) {
+            SettingVersion()
+        }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingAppearance(darkTheme: Boolean, changeTheme: () -> Unit) {
+fun SettingAppearance(changeTheme: () -> Unit) {
+    var checked by rememberSaveable { mutableStateOf(true) }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(color = Color.Gray)
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.surfaceVariant)
             .padding(horizontal = 8.dp, vertical = 12.dp),
     ) {
-        Text(text = "Aparencia", fontSize = 24.sp, color = Color.Black.copy(alpha = 0.3f))
+        Text(
+            text = "Aparencia",
+            color = MaterialTheme.colorScheme.primary,
+            style = MaterialTheme.typography.headlineLarge,
+        )
         Row(
             modifier = Modifier.fillMaxWidth(),
-
-            ) {
-            Text(
-                "modo oscuro",
-                fontSize = 20.sp,
-                color = Color.Black,
-                modifier = Modifier
-                    .weight(5f)
-                    .align(CenterVertically),
-            )
-            var checked by rememberSaveable { mutableStateOf(true) }
+        ) {
+            if (!checked) {
+                Text(
+                    "Modo oscuro",
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .weight(5f)
+                        .align(CenterVertically),
+                )
+            } else {
+                Text(
+                    "Modo claro",
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .weight(5f)
+                        .align(CenterVertically),
+                )
+            }
 
             Switch(
                 checked = checked,
@@ -107,7 +115,6 @@ fun SettingAppearance(darkTheme: Boolean, changeTheme: () -> Unit) {
     }
 }
 
-
 @Composable
 fun SettingVersion() {
     Text(
@@ -118,7 +125,7 @@ fun SettingVersion() {
     )
 }
 
-
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun About() {
     var isExpanded by remember { mutableStateOf(false) }
@@ -126,78 +133,111 @@ fun About() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(color = Color.Gray)
-            .padding(horizontal = 8.dp, vertical = 12.dp),
+            .clip(RoundedCornerShape(16.dp))
+            .background(color = MaterialTheme.colorScheme.surfaceVariant)
+            .padding(horizontal = 8.dp),
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .clickable { isExpanded = !isExpanded }
-                .padding(8.dp),
+                .padding(vertical = 16.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = CenterVertically,
         ) {
-            Text("Acerca de nosotros", fontSize = 24.sp, color = Color.Black.copy(alpha = 0.3f))
+            Text("Conoce nuestro equipo", fontSize = 24.sp)
             Icon(
                 imageVector = if (isExpanded) Icons.Default.ArrowDropUp else Icons.Default.ArrowDropDown,
-                contentDescription = null
+                contentDescription = null,
             )
         }
 
         if (isExpanded) {
-            LazyColumn {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight(0.8f)
+                    .fillMaxWidth(),
+            ) {
                 item {
-                    Card(
+                    ElevatedCard(
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 6.dp,
+                        ),
+                        colors = CardDefaults.elevatedCardColors(
+                            containerColor = MaterialTheme.colorScheme.surface,
+                            contentColor = MaterialTheme.colorScheme.onSurface,
+                        ),
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight()
-                            .clip(RoundedCornerShape(16.dp))
+                            .fillParentMaxHeight(0.98f),
                     ) {
-                        Column(
+                        Row(
                             modifier = Modifier
-                                .padding(16.dp)
                                 .fillMaxWidth()
-                        )
-                        {
+                                .fillParentMaxHeight(0.33f)
+                                .background(Color.Green)
+//                                .combinedClickable(
+//                                    onClick = { },
+//                                    onLongClick = { },
+//                                ),
+                        ) {
                             Text(
-                                text = "¡Bienvenido a TripOut, tu compañero de viaje!",
-                                style = MaterialTheme.typography.titleMedium
+                                text = "TripOut",
+                                style = MaterialTheme.typography.headlineLarge,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(8.dp),
                             )
-                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillParentMaxHeight(0.33f)
+                                .background(Color.Red)
+//                                .combinedClickable(
+//                                    onClick = { },
+//                                    onLongClick = { },
+//                                ),
+                        ) {
                             Text(
-                                text = "En TripOut, nos apasiona descubrir el mundo . Somos alumnos de la Universidad de la Matanza",
-                                style = MaterialTheme.typography.titleMedium
+                                text = "TripOut",
+                                style = MaterialTheme.typography.headlineLarge,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(8.dp),
                             )
-                            Spacer(modifier = Modifier.height(16.dp))
+                        }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillParentMaxHeight(0.33f)
+                                .background(Color.Blue)
+//                                .combinedClickable(
+//                                    onClick = { },
+//                                    onLongClick = { },
+//                                ),
+                        ) {
                             Text(
-                                text = "¿Te preguntas por dónde empezar tu próximo viaje? Deja que TripOut te guíe con sugerencias adaptadas a tus intereses y preferencias. Ya sea que busques escapadas relajantes o aventuras llenas de adrenalina.",
-                                style = MaterialTheme.typography.titleMedium
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Text(
-                                text = "Descubre nuevos horizontes, conéctate con otros apasionados por los viajes y comparte tus propias historias. En TripOut, creemos que cada viaje es una oportunidad para enriquecer nuestras vidas y crear recuerdos duraderos.",
-                                style = MaterialTheme.typography.titleMedium
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Text(
-                                text = "Prepárate para embarcarte en un viaje único con TripOut. ¡Tu próxima aventura comienza aquí!",
-                                style = MaterialTheme.typography.titleMedium
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Text(
-                                text = "¡Vamos juntos a explorar el mundo!",
-                                style = MaterialTheme.typography.titleMedium
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
-                            Text(
-                                text = "Martin, Gabriel y Ariel\nCreadores de TripOut",
-                                style = MaterialTheme.typography.titleMedium
+                                text = "TripOut",
+                                style = MaterialTheme.typography.headlineLarge,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(8.dp),
                             )
                         }
                     }
-
-
                 }
+                /*
+                item {
+                    Text(
+                        text = "Somos un pequeño grupo de estudiantes apasionados por el desarrollo de aplicaciones móviles modernas e intuitivas\n" +
+                            "En TripOut te ofrecemos la posibilidad de planificar tus viajes de la manera mas organizada posible, brindandote todas las herramientas necesarias para cumplir con tus objetivos.\n" +
+                            "Prepárate para embarcarte en un viaje único con TripOut. ¡Tu próxima aventura comienza aquí!\n" +
+                            "Martín, Gabriel y Ariel\n" +
+                            "Equipo de TripOut",
+                        style = MaterialTheme.typography.bodyLarge,
+                    )
+                }
+                 */
             }
         }
     }
