9341ee8011ee631fbc386fa0790deafcb72d3165
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-11-20 14:00:01 -0300

    feat: implement MapViewModelTest

--- app/src/test/java/com/example/turistaapp/map/ui/MapViewModelTest.kt
+++ app/src/test/java/com/example/turistaapp/map/ui/MapViewModelTest.kt
@@ -7,6 +7,7 @@ import com.example.turistaapp.map.domain.GetRouteModel
 import com.example.turistaapp.map.domain.SetIsMapTutorialComplete
 import com.example.turistaapp.map.ui.viewmodel.MapViewModel
 import com.example.turistaapp.my_trips.domain.GetTripsUseCase
+import com.google.android.gms.maps.model.LatLng
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.RelaxedMockK
@@ -54,16 +55,30 @@ class MapViewModelTest {
     }
 
     @Test
-    fun `init & getLastLocation - When getLastLocationUseCase is not null - then lastLocation flow get LastLocation from UseCase` () = runTest{
-        val expected = Location("test").apply {
-            latitude = 1.0
-            longitude = 1.0
-        }
+    fun `init - LastLocation Must is null - when getLastLocationUseCase is null` () = runTest{
 
-//        coEvery { getLastLocationUseCase() } returns expected
+        coEvery { getLastLocationUseCase() } returns null
 
-        val actual = mapViewModel.lastLocation.first()
+        val result = mapViewModel.lastLocation.first()
 
-        assertEquals(expected.latitude, actual?.latitude)
+        //NOTA: Por alguna razon, en vez de dar null da LatLng(0.0,0.0)
+        assertEquals(LatLng(0.0,0.0), result)
     }
+
+    @Test
+    fun `init - LastLocation Must is not null - when getLastLocationUseCase is not null` () = runTest{
+
+        val location = Location("test")
+
+        coEvery { getLastLocationUseCase() } returns location
+
+        val result = mapViewModel.lastLocation.first()
+
+        assertEquals(LatLng(0.0,0.0), result)
+    }
+
+//    @Test
+//    fun `init - destinationLocations Must is not null - When  ` () = runTest{
+//
+//    }
 }
