eb5465199547ede823b10e06d5f957c0735d0b2b
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-11-22 15:15:15 -0300

    feat: ChangeLanguageContent now have a dialog.

--- app/src/main/java/com/example/turistaapp/setting/ui/components/ChangeLanguageContent.kt
+++ app/src/main/java/com/example/turistaapp/setting/ui/components/ChangeLanguageContent.kt
@@ -6,12 +6,19 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Translate
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.turistaapp.setting.data.LanguageApp
@@ -23,11 +30,21 @@ fun ChangeLanguageContent(
     codeLanguage: String,
     onClick: (String) -> Unit,
 ) {
+    var selectedLanguage by remember {
+        mutableStateOf("")
+    }
+
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
+
     LazyRow {
         items(languages) {
             FilterChip(
                 onClick = {
-                    onClick(it.code)
+//                    onClick(it.code)
+                    selectedLanguage = it.code
+                    showDialog = true
                 },
                 label = {
                     Text(it.name)
@@ -45,5 +62,41 @@ fun ChangeLanguageContent(
                 modifier = Modifier.padding(end = 8.dp)
             )
         }
+    }
+
+    if(showDialog){
+        AlertDialog(
+            icon = {
+                Icon(Icons.Default.Translate, contentDescription = Icons.Default.Translate.name)
+            },
+            title = {
+                Text(text = "Cambiar de idioma")
+            },
+            text = {
+                Text(text = "¿Estas seguro de cambiar el idioma?\nAl aceptar se te redirigirá a la pantalla de inicio")
+            },
+            onDismissRequest = {
+                showDialog = false
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        showDialog = false
+                        onClick(selectedLanguage)
+                    }
+                ) {
+                    Text("Confirm")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        showDialog = false
+                    }
+                ) {
+                    Text("Cancel")
+                }
+            }
+        )
     }
 }
